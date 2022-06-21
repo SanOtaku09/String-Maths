39a42976e546c70aca2be6f426ff2adfa9c5f565
sanotaku09 <sanotaku098@gmail.com>
2022-06-21 17:14:17 +0530

    hide exception and meta data class

--- lib/src/main/kotlin/string/maths/exception/IllegalFormatException.kt
+++ lib/src/main/kotlin/string/maths/exception/IllegalFormatException.kt
@@ -1,7 +1,7 @@
 package string.maths.exception
 import string.maths.metaData.DataType
 
-class IllegalFormatException( receivedData : Any ) : Exception (
+internal class IllegalFormatException( receivedData : Any ) : Exception (
     """|
         |Expected : ${DataType.SupportedDataType.string}
         |Received : $receivedData
